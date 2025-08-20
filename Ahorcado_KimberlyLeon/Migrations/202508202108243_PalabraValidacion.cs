f9f34aa31b8889144e7db7d542aa7c47d6d3914f
KimberlyTech <technology@2costaricarealestate.com>
2025-08-20 15:11:57 -0600

    Validación de solo letras

---
+++ Ahorcado_KimberlyLeon/Migrations/202508202108243_PalabraValidacion.cs
@@ -0,0 +1,18 @@
+﻿namespace Ahorcado_KimberlyLeon.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class PalabraValidacion : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Palabras", "Texto", c => c.String(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Palabras", "Texto", c => c.String());
+        }
+    }
+}
--- Ahorcado_KimberlyLeon/Models/Palabra.cs
+++ Ahorcado_KimberlyLeon/Models/Palabra.cs
@@ -6,7 +6,11 @@ namespace Ahorcado_KimberlyLeon.Models
     {
         [Key]
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "La palabra es obligatoria.")]
+        [RegularExpression(@"^[a-zA-ZáéíóúÁÉÍÓÚñÑ]+$", ErrorMessage = "La palabra solo puede contener letras.")]
         public string Texto { get; set; }
+
         public bool Usada { get; set; }
     }
 }
